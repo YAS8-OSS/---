072fae9d2afe33f4d7db6c4299606c559a9f61ce
يسوم <wowli2009@gmail.com>
2025-07-23 00:56:47 +0300

    Create manhwa-data.js

---
+++ manhwa-data.js
@@ -0,0 +1,21 @@
+const manhwas = [
+  {
+    id: 1,
+    title: "مانهوا الغموض",
+    description: "قصة مليانة أحداث مشوقة وغموض.",
+    cover: "https://i.imgur.com/O6qhzAc.jpg",
+    chapters: [
+      { number: 1, images: ["https://i.imgur.com/a1.jpg", "https://i.imgur.com/a2.jpg"] },
+      { number: 2, images: ["https://i.imgur.com/b1.jpg", "https://i.imgur.com/b2.jpg"] },
+    ],
+  },
+  {
+    id: 2,
+    title: "مانهوا المغامرات",
+    description: "رحلة بطلنا في عالم مليان مغامرات خطيرة.",
+    cover: "https://i.imgur.com/RkjTt7T.jpg",
+    chapters: [
+      { number: 1, images: ["https://i.imgur.com/c1.jpg", "https://i.imgur.com/c2.jpg"] },
+    ],
+  },
+];
